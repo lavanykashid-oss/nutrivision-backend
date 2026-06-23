f5a58ea100e164d5a2815bf2833ebe9e12ae12a3
Lavany Sharad Kashid <lavanykashid@gmail.com>
2026-06-23 22:57:57 +0530

    dashboard route

--- app/routes/food_routes.py
+++ app/routes/food_routes.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models.meal import Meal
 from app.config.database import db
 from app.models.nutrition_analysis import NutritionAnalysis
+from sqlalchemy import func
 
 food_bp = Blueprint(
     "food",
@@ -99,8 +100,117 @@ def get_history():
             "calories": analysis.calories if analysis else 0,
             "protein": analysis.protein if analysis else 0,
             "carbs": analysis.carbs if analysis else 0,
-            "fat": analysis.fat if analysis else 0
+            "fat": analysis.fat if analysis else 0,
+
+            "fiber": analysis.fiber if analysis else 0,
+            "sugar": analysis.sugar if analysis else 0,
+            "sodium": analysis.sodium if analysis else 0,
+
+            "vitamins": analysis.vitamin_data if analysis else []
+
             
         })
 
     return jsonify(history)
+
+@food_bp.route("/dashboard", methods=["GET"])
+@jwt_required()
+def dashboard():
+
+    user_id = get_jwt_identity()
+
+    meals = Meal.query.filter_by(
+        user_id=user_id
+    ).all()
+
+    total_calories = 0
+    total_protein = 0
+    total_carbs = 0
+    total_fat = 0
+    total_fiber = 0
+    total_sugar = 0
+    total_sodium = 0
+
+    health_scores = []
+
+    recent_meals = []
+
+    weekly_data = [
+    {"day": "Mon", "calories": 0, "protein": 0},
+    {"day": "Tue", "calories": 0, "protein": 0},
+    {"day": "Wed", "calories": 0, "protein": 0},
+    {"day": "Thu", "calories": 0, "protein": 0},
+    {"day": "Fri", "calories": 0, "protein": 0},
+    {"day": "Sat", "calories": 0, "protein": 0},
+    {"day": "Sun", "calories": 0, "protein": 0},
+]
+
+    for meal in meals:
+
+        analysis = NutritionAnalysis.query.filter_by(
+            meal_id=meal.id
+        ).first()
+
+        if analysis:
+
+            score = 100
+
+            if analysis.calories and analysis.calories > 500:
+                  score -= 10
+
+            if analysis.sugar and analysis.sugar > 25:
+                   score -= 10
+
+            if analysis.fiber and analysis.fiber > 3:
+              score += 5
+
+            score = min(score,100)
+            health_scores.append(score)
+
+
+            
+
+            total_calories += analysis.calories or 0
+            total_protein += analysis.protein or 0
+            total_carbs += analysis.carbs or 0
+            total_fat += analysis.fat or 0
+            total_fiber += analysis.fiber or 0
+            total_sugar += analysis.sugar or 0
+            total_sodium += analysis.sodium or 0
+
+            day_index = meal.created_at.weekday()
+
+            weekly_data[day_index]["calories"] += analysis.calories or 0
+            weekly_data[day_index]["protein"] += analysis.protein or 0
+
+            recent_meals.append({
+                "meal_name": meal.meal_name,
+                "calories": analysis.calories,
+                "protein": analysis.protein
+            })
+
+    avg_health_score = (
+        round(sum(health_scores) / len(health_scores))
+        if health_scores
+        else 0
+)
+
+   
+
+    return jsonify({
+        "total_calories": total_calories,
+        "total_protein": total_protein,
+        "total_carbs": total_carbs,
+        "total_fat": total_fat,
+        "total_fiber": total_fiber,
+        "total_sugar": total_sugar,
+        "total_sodium": total_sodium,
+
+        "total_meals": len(meals),
+        "recent_meals": recent_meals[-5:],
+        "weekly_data": weekly_data,
+
+        "health_score": avg_health_score
+
+        
+    })
